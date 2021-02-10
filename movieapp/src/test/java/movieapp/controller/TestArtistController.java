cfa0537c6619d3ca42026f084c3112f7b7fcef60
Matthias Colin <matthias.colin@gmail.com>
2021-02-10 12:35:32 +0100

    end day 10 morning

---
+++ movieapp/src/test/java/movieapp/controller/TestArtistController.java
@@ -0,0 +1,15 @@
+package movieapp.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class TestArtistController {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
