6d91bb7cbaf4695a877e2f5e93e906ba56c60d5b
Matthias Colin <matthias.colin@gmail.com>
2021-02-10 13:19:24 +0100

    test u controller

--- movieapp/src/main/java/movieapp/controller/ArtistController.java
+++ movieapp/src/main/java/movieapp/controller/ArtistController.java
@@ -1,8 +1,16 @@
 package movieapp.controller;
 
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import movieapp.dto.ArtistSimple;
+import movieapp.service.IArtistService;
 
 
 
@@ -10,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/artists")
 public class ArtistController {
+	@Autowired
+	private IArtistService artistService;
 	
+	
+	@GetMapping("/{id}")
+	Optional<ArtistSimple> getBydId(@PathVariable("id") int id){
+		return artistService.getById(id);
+	}
 
 }
--- movieapp/src/test/java/movieapp/controller/TestArtistController.java
+++ movieapp/src/test/java/movieapp/controller/TestArtistController.java
@@ -1,15 +1,77 @@
 package movieapp.controller;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.eq;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.jayway.jsonpath.JsonPath;
 
+import movieapp.dto.ArtistSimple;
+import movieapp.dto.MovieSimple;
+import movieapp.service.IArtistService;
+
+@WebMvcTest(ArtistController.class)
 class TestArtistController {
+	private static final String BASE_URI = "/api/artists";
+	
+	@MockBean
+	IArtistService artistService;
+	
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetByIdPresent() throws Exception {
+		// 1. given
+		int id = 1;
+		String name = "Will Smith";
+		LocalDate birthdate = LocalDate.of(1968, 9, 25);
+		ArtistSimple artistMock = new ArtistSimple(id, name, birthdate);
+		given(artistService.getById(id))
+			.willReturn(Optional.of(artistMock));
+		// 2. when
+		mockMvc.perform(MockMvcRequestBuilders
+				.get(BASE_URI + "/" + id)
+				.accept(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.id").exists())
+			.andExpect(jsonPath("$.id").value(id))
+			.andExpect(jsonPath("$.name").value(name))
+			.andExpect(jsonPath("$.birthdate").value(birthdate.toString()));
+	}
+	
+	@Test
+	void testGetByIdMissing() throws Exception {
+		// 1. given
+		int id = 1;
+		given(artistService.getById(id))
+			.willReturn(Optional.empty());
+		// 2. when
+		mockMvc.perform(MockMvcRequestBuilders
+				.get(BASE_URI + "/" + id)
+				.accept(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$").doesNotExist());
 	}
 
 }
