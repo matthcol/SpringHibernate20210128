9c7fc2d67dd24352ece01c82cc2233b85552510c
Matthias Colin <matthias.colin@gmail.com>
2021-02-15 17:05:17 +0100

    repo + tu mongo repo movie

--- movieapp/src/main/java/movieapp/persistence/mongodb/MovieRepository.java
+++ movieapp/src/main/java/movieapp/persistence/mongodb/MovieRepository.java
@@ -1,9 +1,17 @@
 package movieapp.persistence.mongodb;
 
+import java.util.stream.Stream;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import movieapp.persistence.document.Movie;
 
 public interface MovieRepository extends MongoRepository<Movie, Integer>{
+	
+	Stream<Movie> findByTitle(String title);
+	Stream<Movie> findByDirectorName(String name);
+	Page<Movie> findByYearGreaterThanOrderByYear(int year, Pageable pageable);
 
 }
--- movieapp/src/test/java/movieapp/persistence/document/TestMovieRepository.java
+++ movieapp/src/test/java/movieapp/persistence/document/TestMovieRepository.java
@@ -2,12 +2,14 @@ package movieapp.persistence.document;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.domain.PageRequest;
@@ -48,7 +50,7 @@ class TestMovieRepository {
 	@ParameterizedTest
 	@MethodSource("numberMovie")
 	void testCreateFranchise(int number) {
-		var movie = Movie.of("Star Wars " + number, 1977);
+		var movie = Movie.of("Star Wars " + number, 1976+number);
 		// save in repo mongoDb
 		movieRepository.save(movie);
 		System.out.println("After saving: " + movie);
@@ -60,9 +62,29 @@ class TestMovieRepository {
 		System.out.println(movies);
 	}
 	
+	@ParameterizedTest
+	@ValueSource(ints = {0,1,19})
+	void testFindAllPagination(int numPage) {
+		int size = 5;
+		var movies = movieRepository.findAll(PageRequest.of(numPage, size))
+				.map(Movie::getTitle)
+				.toList();
+		System.out.println(movies);
+	}
+	
 	@Test
-	void testFindAllPagination() {
-		var movies = movieRepository.findAll(PageRequest.of(0, 5))
+	void testFindByDirectorName() {
+		var movies = movieRepository.findByDirectorName("Ridley Scott")
+				.collect(Collectors.toList());
+		System.out.println(movies);
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = {0,1,19})
+	void testFindByYearGreaterThanOrderByYear(int numPage) {
+		int size = 5;
+		var movies = movieRepository.findByYearGreaterThanOrderByYear(1980, PageRequest.of(numPage, size))
+				.map(Movie::getYear)
 				.toList();
 		System.out.println(movies);
 	}
